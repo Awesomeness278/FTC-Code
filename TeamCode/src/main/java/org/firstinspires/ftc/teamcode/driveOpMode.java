13214b07f1344ec97267ae803a94ca27c9549989
Awesomeness278 <tylerbalota@gmail.com>
2020-09-22 20:25:30 -0500

    Added one servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
@@ -2,11 +2,16 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
 import org.firstinspires.ftc.teamcode.Vector2d;
 
 @TeleOp
 public class driveOpMode extends LinearOpMode{
     private DcMotor[] motors = new DcMotor[4];
+    private Servo servo;
+    private double servoPower;
     private double[] tgtPowers = new double[4];
     double tgtPower0 = 0;
     double tgtPower1 = 0;
@@ -18,11 +23,13 @@ public class driveOpMode extends LinearOpMode{
         for(int i = 0; i < 4; i++){
             motors[i] = hardwareMap.get(DcMotor.class, "Motor "+i);
         }
+        servo = hardwareMap.get(Servo.class,"servo");
         telemetry.addData("Status","Initialized");
         telemetry.update();
         waitForStart();
         while(opModeIsActive()) {
             dir = new Vector2d(this.gamepad1.left_stick_x,-this.gamepad1.left_stick_y);
+            servoPower = -gamepad1.right_stick_y;
             tgtPowers[0] = this.gamepad1.left_stick_y;
             tgtPowers[1] = -this.gamepad1.left_stick_y;
             tgtPowers[2] = -this.gamepad1.left_stick_y;
@@ -30,6 +37,7 @@ public class driveOpMode extends LinearOpMode{
             for(int i = 0; i < 4; i++){
                 motors[i].setPower(tgtPowers[i]);
             }
+            servo.setPosition(servoPower);
             telemetry.addData("Status", "Running");
             telemetry.addData("Direction","X: "+dir.x+", Y: "+dir.y);
             telemetry.update();
