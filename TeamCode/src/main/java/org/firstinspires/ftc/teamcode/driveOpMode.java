d25f706c9a5e92d49d6efd6dd387c25cad871b59
Awesomeness278 <tylerbalota@gmail.com>
2020-09-24 19:56:42 -0500

    Added basic forward/backward drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
@@ -11,6 +13,8 @@ import org.firstinspires.ftc.teamcode.Vector2d;
 public class driveOpMode extends LinearOpMode{
     private DcMotor[] motors = new DcMotor[4];
     private Servo servo;
+    private ColorRangeSensor color;
+    private UltrasonicSensor distance;
     private double servoPower;
     private double[] tgtPowers = new double[4];
     double tgtPower0 = 0;
@@ -23,13 +27,17 @@ public class driveOpMode extends LinearOpMode{
         for(int i = 0; i < 4; i++){
             motors[i] = hardwareMap.get(DcMotor.class, "Motor "+i);
         }
+        double dist = distance.getUltrasonicLevel();
         servo = hardwareMap.get(Servo.class,"servo");
+        color = hardwareMap.get(ColorRangeSensor.class, "colorRangeSensor");
+        NormalizedRGBA sensedColor = color.getNormalizedColors();
+        NormalizedRGBA targetColor = new NormalizedRGBA();
         telemetry.addData("Status","Initialized");
         telemetry.update();
         waitForStart();
         while(opModeIsActive()) {
             dir = new Vector2d(this.gamepad1.left_stick_x,-this.gamepad1.left_stick_y);
-            servoPower = -gamepad1.right_stick_y;
+            servoPower = gamepad1.right_stick_y+1;
             tgtPowers[0] = this.gamepad1.left_stick_y+Math.min(this.gamepad1.left_stick_x,0);
             tgtPowers[1] = this.gamepad1.left_stick_y+Math.min(this.gamepad1.left_stick_x,0);
             tgtPowers[2] = -this.gamepad1.left_stick_y+Math.max(this.gamepad1.left_stick_x,0);
