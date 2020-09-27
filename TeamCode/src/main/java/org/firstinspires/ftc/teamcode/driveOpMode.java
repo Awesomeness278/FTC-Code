50afdd6a7cbfb4de53e8ac761d47414e398922eb
Awesomeness278 <tylerbalota@gmail.com>
2020-09-27 12:54:16 -0500

    Added basic forward/backward drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
@@ -14,7 +14,6 @@ public class driveOpMode extends LinearOpMode{
     private DcMotor[] motors = new DcMotor[4];
     private Servo servo;
     private ColorRangeSensor color;
-    private UltrasonicSensor distance;
     private double servoPower;
     private double[] tgtPowers = new double[4];
     double tgtPower0 = 0;
@@ -24,18 +23,21 @@ public class driveOpMode extends LinearOpMode{
     Vector2d dir;
     @Override
     public void runOpMode(){
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 1; i++){
             motors[i] = hardwareMap.get(DcMotor.class, "Motor "+i);
         }
-        double dist = distance.getUltrasonicLevel();
         servo = hardwareMap.get(Servo.class,"servo");
         color = hardwareMap.get(ColorRangeSensor.class, "colorRangeSensor");
-        NormalizedRGBA sensedColor = color.getNormalizedColors();
-        NormalizedRGBA targetColor = new NormalizedRGBA();
         telemetry.addData("Status","Initialized");
         telemetry.update();
         waitForStart();
         while(opModeIsActive()) {
+            NormalizedRGBA sensedColor = color.getNormalizedColors();
+            telemetry.addData("tgtCol",(sensedColor.red+sensedColor.blue+sensedColor.green)/3);
+            if((sensedColor.red+sensedColor.blue+sensedColor.green)/3 > 125){
+                telemetry.addData("DETECTED","WHITE");
+                motors[0].setPower(1);
+            }
             dir = new Vector2d(this.gamepad1.left_stick_x,-this.gamepad1.left_stick_y);
             servoPower = gamepad1.right_stick_y+1;
             tgtPowers[0] = this.gamepad1.left_stick_y+Math.min(this.gamepad1.left_stick_x,0);
