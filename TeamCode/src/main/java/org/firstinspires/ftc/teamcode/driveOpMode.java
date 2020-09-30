4dd53d5ea449289dd50f90c4fdad99c9182f0442
Awesomeness278 <tylerbalota@gmail.com>
2020-09-29 20:31:58 -0500

    Added basic forward/backward left/right drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveOpMode.java
@@ -3,6 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
@@ -13,6 +14,8 @@ public class driveOpMode extends LinearOpMode{
     private ColorRangeSensor color;
     private double servoPower;
     private double[] tgtPowers = new double[4];
+    int fix = 0;
+    int fix2 = 0;
     double leftPower;
     double rightPower;
     Vector dir;
@@ -21,16 +24,19 @@ public class driveOpMode extends LinearOpMode{
         for(int i = 0; i < 2; i++){
             motors[i] = hardwareMap.get(DcMotor.class, "Motor "+(2*i));
         }
+        motors[0].setDirection(DcMotorSimple.Direction.REVERSE);
         //servo = hardwareMap.get(Servo.class,"servo");
         //color = hardwareMap.get(ColorRangeSensor.class, "colorRangeSensor");
         telemetry.addData("Status","Initialized");
         telemetry.update();
         waitForStart();
+        motors[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motors[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         while(opModeIsActive()) {
             //NormalizedRGBA sensedColor = color.getNormalizedColors();
             dir = new Vector(this.gamepad1.left_stick_x,-this.gamepad1.left_stick_y);
-            double drive = -gamepad1.left_stick_y;
-            double drive2  =  gamepad1.right_stick_y;
+            double drive = gamepad1.left_stick_y/2;
+            double drive2 = gamepad1.right_stick_y/2;
             tgtPowers[0] = drive;
             tgtPowers[1] = drive;
             tgtPowers[2] = drive2;
@@ -38,10 +44,27 @@ public class driveOpMode extends LinearOpMode{
             for(int i = 0; i < 2; i++){
                 motors[i].setPower(tgtPowers[2*i]);
             }
-            //servo.setPosition(servoPower);
+            telemetry.addData("Zero Power Behavior",motors[0].getZeroPowerBehavior());
             telemetry.addData("Status", "Running");
             telemetry.addData("Direction","X: "+dir.x+", Y: "+dir.y);
             telemetry.update();
+            if(gamepad1.a){
+                if(fix==0) {
+                    motors[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                    motors[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                    fix = 2;
+                }else if(fix == 1){
+                    motors[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    motors[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    fix = 2;
+                }
+            }
+            if(!gamepad1.a && motors[0].getZeroPowerBehavior()== DcMotor.ZeroPowerBehavior.BRAKE){
+                fix = 0;
+            }
+            if(!gamepad1.a && motors[0].getZeroPowerBehavior()== DcMotor.ZeroPowerBehavior.FLOAT){
+                fix = 1;
+            }
         }
     }
 }
