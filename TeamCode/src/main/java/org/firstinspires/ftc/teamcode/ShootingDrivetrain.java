6576c2ad041dd3665a5a95cd77b0491bde769726
Platypus10 <charsmitty8@gmail.com>
2021-01-19 17:00:42 -0600

    Allowed the intake and conveyor to move with 1 button press and added more functionality to the arm.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingDrivetrain.java
@@ -44,17 +44,13 @@ public class ShootingDrivetrain extends LinearOpMode {
 
         int flywheelSwitch = 0;
         int armTargetPosition = 0;
-        int vertArmPos = arm.getCurrentPosition()-123;
-        int horArmPos = arm.getCurrentPosition()-272;
-        int homeArmPos = arm.getCurrentPosition();
+        int vertArmPos = 0;
+        int horArmPos = -245;
         boolean gripToggle = false;
-        int timeSinceLeftBumper = 0;
         waitForStart();
         while (opModeIsActive()) {
 
-            arm.setPower(0.25);
-
-            timeSinceLeftBumper--;
+            arm.setPower(0.15);
 
             //Movement
 
@@ -145,34 +141,42 @@ public class ShootingDrivetrain extends LinearOpMode {
             shooter.setPower(-(flywheelSwitch * flywheelPower));
 
             //Intake and Conveyor
-
-            if (gamepad2.a) {
+            if(gamepad2.a){
                 intake.setPower(1);
-            } else {
+            }else{
                 intake.setPower(0);
             }
             if (gamepad2.b) {
                 conveyor.setPower(0.7);
+                intake.setPower(1);
             } else {
                 conveyor.setPower(0);
+                intake.setPower(0);
             }
             //Arm
+            if(gamepad1.y){
+                armTargetPosition -= 1;
+                arm.setTargetPosition(armTargetPosition);
+            }
+            if(gamepad1.x){
+                armTargetPosition += 1;
+                arm.setTargetPosition(armTargetPosition);
+            }
+            if(gamepad2.y){
+                armTargetPosition = -215;
+                arm.setTargetPosition(armTargetPosition);
+            }
             if(gamepad2.left_bumper && !leftBumperPressed){
-                if(timeSinceLeftBumper>0){
-                    armTargetPosition = homeArmPos;
-                }else{
-                    if (armTargetPosition == vertArmPos) {
-                        armTargetPosition = horArmPos;
-                    } else {
-                        armTargetPosition = vertArmPos;
-                    }
-                    timeSinceLeftBumper = 50;
+                if (armTargetPosition == vertArmPos) {
+                    armTargetPosition = horArmPos;
+                } else {
+                    armTargetPosition = vertArmPos;
                 }
                 leftBumperPressed = true;
+                arm.setTargetPosition(armTargetPosition);
             }else if(!gamepad2.left_bumper){
                 leftBumperPressed = false;
             }
-            arm.setTargetPosition(armTargetPosition);
 
             if (gamepad2.x) {
                 if (!xPressed) {
@@ -181,7 +185,7 @@ public class ShootingDrivetrain extends LinearOpMode {
                 xPressed = true;
             } else xPressed = false;
 
-            if (gripToggle == true) {
+            if (gripToggle) {
                 grip.setPosition(-0.15);
             } else {
                 grip.setPosition(0.25);
