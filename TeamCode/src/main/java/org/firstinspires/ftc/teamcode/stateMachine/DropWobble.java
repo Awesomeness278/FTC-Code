bb7dd7b59c09af4e3eface21b45cb1871d651225
Awesomeness278 <tylerbalota@gmail.com>
2021-02-02 14:56:57 -0600

    Final Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/DropWobble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/DropWobble.java
@@ -3,13 +3,15 @@ package org.firstinspires.ftc.teamcode.stateMachine;
 public class DropWobble extends StateManager {
     @Override
     public void Run(StateMachine machine) {
-        machine.opMode.Arm.setPower(-0.15);
+        machine.opMode.Arm.setPower(0.1);
+        machine.opMode.Claw.setPosition(0);
         machine.opMode.Arm.setTargetPosition(-245);
         double startWait = machine.opMode.getRuntime();
         while(machine.opMode.getRuntime()-startWait<2){
         }
         machine.opMode.Claw.setPosition(1);
         startWait = machine.opMode.getRuntime();
+        machine.opMode.Arm.setTargetPosition(0);
         while(machine.opMode.getRuntime()-startWait<1){
         }
         Exit(machine);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/wait.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/wait.java
@@ -4,17 +4,21 @@ public class wait extends StateManager {
     public wait(){}
     @Override
     public void Run(StateMachine machine) {
-        double delay = 2;
+        double delay = 2.5;
         double time = 0.2;
+        int rings = 3;
+        boolean updated = false;
+        boolean shoot = false;
+        double currentTime = machine.opMode.getRuntime();
         while(!ExitCondition(machine)&&machine.opMode.opModeIsActive()) {
             double leftFront = 0;
             double rightFront = 0;
             double leftBack = 0;
             double rightBack = 0;
-            leftFront += -machine.opMode.odometry.returnOrientation()/10;
-            leftBack += -machine.opMode.odometry.returnOrientation()/10;
-            rightFront += machine.opMode.odometry.returnOrientation()/10;
-            rightBack += machine.opMode.odometry.returnOrientation()/10;
+            leftFront += -machine.opMode.odometry.returnOrientation()/7.5;
+            leftBack += -machine.opMode.odometry.returnOrientation()/7.5;
+            rightFront += machine.opMode.odometry.returnOrientation()/7.5;
+            rightBack += machine.opMode.odometry.returnOrientation()/7.5;
             double scalar = Math.max(Math.max(Math.abs(leftFront), Math.abs(leftBack)), Math.max(Math.abs(rightFront), Math.abs(rightBack)));
             if (scalar < 1) scalar = 1;
             machine.opMode.left_front.setPower(leftFront / scalar * 0.1);
@@ -22,10 +26,22 @@ public class wait extends StateManager {
             machine.opMode.right_front.setPower(rightFront / scalar * 0.1);
             machine.opMode.right_back.setPower(rightBack / scalar * 0.1);
             machine.opMode.Shooter.setPower(-1);
-            if (machine.opMode.getRuntime() - time < machine.opMode.getRuntime() - machine.opMode.getRuntime() % (time+delay)) {
+            if(machine.opMode.getRuntime()-currentTime>1){
+                currentTime = machine.opMode.getRuntime();
+                shoot = true;
+            }
+            if ((machine.opMode.getRuntime()-currentTime) - time < (machine.opMode.getRuntime()-currentTime) - (machine.opMode.getRuntime()-currentTime) % (time+delay) && shoot) {
                 machine.opMode.Conveyor.setPower(1);
+                if(!updated){
+                    rings--;
+                    updated=true;
+                }
             } else {
                 machine.opMode.Conveyor.setPower(0);
+                updated = false;
+                if(rings == 0){
+                    break;
+                }
             }
         }
         machine.opMode.Shooter.setPower(0);
