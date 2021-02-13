aa32063ce9e861867c5ae9fbc86abe125a759c76
Platypus10 <charsmitty8@gmail.com>
2021-02-13 13:55:31 -0600

    Added MoveGrip to manually move the grip by pressing a or b

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveGrip.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveGrip.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Move Grip")
 public class MoveGrip extends LinearOpMode {
     Servo grip;
-    double pos = 0.25;
+    double pos = 0.15;
 
     @Override
     public void runOpMode() {
