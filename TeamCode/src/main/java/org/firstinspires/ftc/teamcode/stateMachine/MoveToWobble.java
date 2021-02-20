a53f62477595796f55e9f01c1458227d8f3ecb4e
Awesomeness278 <tylerbalota@gmail.com>
2021-02-20 14:39:40 -0600

    Better move code?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/MoveToWobble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/MoveToWobble.java
@@ -8,15 +8,15 @@ public class MoveToWobble extends StateManager {
     public void Run(StateMachine machine) {
         Autonomous opMode = machine.opMode;
 
-        machine.addState(States.Move1, new MoveTest(AutonomousData.getInstance().getDodgeRingXPosition(), 36, States.Move2,false));
+        machine.addState(States.Move1, new OldMoveCode(AutonomousData.getInstance().getDodgeRingXPosition(), 36, States.Move2,false));
         if (opMode.recognition != null) {
             if (opMode.recognition.getLabel().equals("Quad")) {
-                machine.addState(States.Move2, new MoveTest(AutonomousData.getInstance().getFourRingXPosition(), 102, States.Rotate,false));
+                machine.addState(States.Move2, new MoveTest(AutonomousData.getInstance().getFourRingXPosition(), 102, States.Rotate));
             } else if (opMode.recognition.getLabel().equals("Single")) {
-                machine.addState(States.Move2, new MoveTest(AutonomousData.getInstance().getOneRingXPosition(), 78, States.Rotate,false));
+                machine.addState(States.Move2, new OldMoveCode(AutonomousData.getInstance().getOneRingXPosition(), 78, States.Rotate,false));
             }
         } else {
-            machine.addState(States.Move2, new MoveTest(AutonomousData.getInstance().getNoRingXPosition(), 56, States.Rotate,false));
+            machine.addState(States.Move2, new OldMoveCode(AutonomousData.getInstance().getNoRingXPosition(), 56, States.Rotate,false));
         }
 
         Exit(machine);
