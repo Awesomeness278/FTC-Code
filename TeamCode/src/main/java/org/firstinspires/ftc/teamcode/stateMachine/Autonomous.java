f9dc364c8caf07a1466e0d1f578a92e677899fcd
Awesomeness278 <tylerbalota@gmail.com>
2021-04-08 20:40:45 -0500

    fixed conveyor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachine/Autonomous.java
@@ -312,8 +312,7 @@ public class Autonomous extends LinearOpMode {
         Arm.setTargetPosition(0);
         Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         Arm.setDirection(DcMotorSimple.Direction.REVERSE);
-        Conveyor.setTargetPosition(Conveyor.getCurrentPosition());
-        Conveyor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Conveyor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         //Conveyor.setTargetPosition(0);
         //Conveyor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
