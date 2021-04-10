9bf54f3edad8dc2b1c5a452698d255c27e5830f7
Awesomeness278 <tylerbalota@gmail.com>
2021-04-10 16:24:19 -0500

    did REV's calculations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingDrivetrain2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingDrivetrain2.java
@@ -20,7 +20,7 @@ public class ShootingDrivetrain2 extends LinearOpMode {
     Servo grip, blocker, straightener, parker;
     //Hardware Map Names for drive motors and odometry wheels.
     String rfName = "Right Front Motor", rbName = "Right Back Motor", lfName = "Left Front Motor", lbName = "Left Back Motor", shootName = "Shooter";
-    PIDFCoefficients coefficients = new PIDFCoefficients(60,0,0,0);
+    PIDFCoefficients coefficients = new PIDFCoefficients(1.17025,0.117025,0,11.7025);
 
     @Override
     public void runOpMode() throws InterruptedException {
